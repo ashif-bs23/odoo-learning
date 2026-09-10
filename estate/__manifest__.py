c42cb3bbf6a3676fa6cf30cd1023d4d7bedb065b
Ashif Iqbal <ashif.iqubal@brainstation-23.com>
2026-09-10 19:20:41 +0600

    xlsx download feature in the estate module

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,12 +2,13 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'Real Estate',
-    'version': '1.3',
+    'version': '1.4',
     'summary': 'Real Estate app',
-    'depends': ['base'],
+    'depends': ['base', 'report_xlsx'],
     'application': True,
     'data': [
         'security/ir.model.access.csv',
+        'report/estate_property_xlsx_views.xml',
         'views/estate_property_views.xml',
         'views/estate_property_offer_views.xml',
         'views/estate_property_type_views.xml',
