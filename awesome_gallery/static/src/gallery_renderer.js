0ba4a95d4b1ef3a4a45d74baeb66a180d4619541
Ashif Iqbal <ashif.iqubal@brainstation-23.com>
2026-09-17 16:41:17 +0600

    Gallery view development : image added

--- awesome_gallery/static/src/gallery_renderer.js
+++ awesome_gallery/static/src/gallery_renderer.js
@@ -1,9 +1,19 @@
 import { Component } from "@odoo/owl";
+import { url } from "@web/core/utils/urls";
 
 export class GalleryRenderer extends Component {
     static template = "awesome_gallery.GalleryRenderer";
     static props = {
         records: { type: Array },
         imageField: { type: String },
+        resModel: { type: String },
     };
+
+    imageUrl(record) {
+        return url("/web/image", {
+            model: this.props.resModel,
+            id: record.id,
+            field: this.props.imageField,
+        });
+    }
 }
