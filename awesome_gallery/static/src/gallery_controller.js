f197b5755621cf6a4c3bd641cd0765034585a364
Ashif Iqbal <ashif.iqubal@brainstation-23.com>
2026-09-17 13:17:38 +0600

    Gallery view development : new view type (gallery) added

---
+++ awesome_gallery/static/src/gallery_controller.js
@@ -0,0 +1,8 @@
+import { Component } from "@odoo/owl";
+import { standardViewProps } from "@web/views/standard_view_props";
+
+export class GalleryController extends Component {
+    static template = "awesome_gallery.GalleryController";
+    static props = { ...standardViewProps };
+
+}
