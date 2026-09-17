60091941831eb2b83ac00c84a8de4d421d6e2b6b
Ashif Iqbal <ashif.iqubal@brainstation-23.com>
2026-09-17 13:56:35 +0600

    Gallery view development : fetch records from the database

--- awesome_gallery/static/src/gallery_controller.js
+++ awesome_gallery/static/src/gallery_controller.js
@@ -1,5 +1,6 @@
-import { Component } from "@odoo/owl";
+import { Component, onWillStart, onWillUpdateProps, useState } from "@odoo/owl";
 import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
 import { standardViewProps } from "@web/views/standard_view_props";
 
 export class GalleryController extends Component {
@@ -9,4 +10,25 @@ export class GalleryController extends Component {
         archInfo: { type: Object },
     };
     static components = { Layout };
+
+    setup() {
+        this.orm = useService("orm");
+        this.model = useState({ records: [] });
+
+        onWillStart(() => this.loadImages(this.props.domain));
+        onWillUpdateProps((nextProps) => this.loadImages(nextProps.domain));
+    }
+
+    async loadImages(domain) {
+        const imageField = this.props.archInfo.imageField;
+        const { records } = await this.orm.webSearchRead(this.props.resModel, domain, {
+            specification: {
+                [imageField]: {},
+            },
+            context: {
+                bin_size: true,
+            },
+        });
+        this.model.records = records;
+    }
 }
