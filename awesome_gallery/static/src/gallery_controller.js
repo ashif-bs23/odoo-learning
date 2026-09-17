42f6b6f95966994393fa318a45c9cc45b44b200b
Ashif Iqbal <ashif.iqubal@brainstation-23.com>
2026-09-17 16:36:38 +0600

    Gallery view development : putting Model and Renderer on the view object and pass them in as props, so another module can swap the renderer

--- awesome_gallery/static/src/gallery_controller.js
+++ awesome_gallery/static/src/gallery_controller.js
@@ -2,20 +2,20 @@ import { Component, onWillStart, onWillUpdateProps, useState } from "@odoo/owl";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
 import { standardViewProps } from "@web/views/standard_view_props";
-import { GalleryModel } from "./gallery_model";
-import { GalleryRenderer } from "./gallery_renderer";
 
 export class GalleryController extends Component {
     static template = "awesome_gallery.GalleryController";
     static props = {
         ...standardViewProps,
         archInfo: { type: Object },
+        Model: Function,
+        Renderer: Function,
     };
-    static components = { Layout, GalleryRenderer };
+    static components = { Layout };
 
     setup() {
         this.orm = useService("orm");
-        this.model = useState(new GalleryModel(this.orm));
+        this.model = useState(new this.props.Model(this.orm));
 
         onWillStart(() => this.load(this.props));
         onWillUpdateProps((nextProps) => this.load(nextProps));
--- awesome_gallery/static/src/gallery_view.js
+++ awesome_gallery/static/src/gallery_view.js
@@ -1,6 +1,8 @@
 import { registry } from "@web/core/registry";
 import { GalleryArchParser } from "./gallery_arch_parser";
 import { GalleryController } from "./gallery_controller";
+import { GalleryModel } from "./gallery_model";
+import { GalleryRenderer } from "./gallery_renderer";
 
 export const galleryView = {
     type: "gallery",
@@ -9,11 +11,15 @@ export const galleryView = {
     multiRecord: true,
     ArchParser: GalleryArchParser,
     Controller: GalleryController,
+    Model: GalleryModel,
+    Renderer: GalleryRenderer,
     props: (genericProps, view) => {
         const { arch } = genericProps;
         const archInfo = new view.ArchParser().parse(arch);
         return {
             ...genericProps,
+            Model: view.Model,
+            Renderer: view.Renderer,
             archInfo,
         };
     },
