aae253227690022729c70db6302a239150a574fd
Ashif Iqbal <ashif.iqubal@brainstation-23.com>
2026-09-17 16:49:10 +0600

    Gallery view development : click to open the contact form

--- awesome_gallery/static/src/gallery_renderer.js
+++ awesome_gallery/static/src/gallery_renderer.js
@@ -1,4 +1,5 @@
 import { Component } from "@odoo/owl";
+import { useService } from "@web/core/utils/hooks";
 import { url } from "@web/core/utils/urls";
 
 export class GalleryRenderer extends Component {
@@ -9,11 +10,19 @@ export class GalleryRenderer extends Component {
         resModel: { type: String },
     };
 
+    setup() {
+        this.action = useService("action");
+    }
+
     imageUrl(record) {
         return url("/web/image", {
             model: this.props.resModel,
             id: record.id,
             field: this.props.imageField,
         });
+    }
+
+    onImageClick(record) {
+        this.action.switchView("form", { resId: record.id });
     }
 }
